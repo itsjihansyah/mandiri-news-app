db93bb860d2a74d14bc6f391bf8acc6d6fd15ea2
Jihan Syahira <111284378+itsjihansyah@users.noreply.github.com>
2025-04-05 21:27:45 +0700

    Hide the API Key

--- app/src/main/java/com/example/mandirinewsapp/util/Constants.kt
+++ app/src/main/java/com/example/mandirinewsapp/util/Constants.kt
@@ -2,7 +2,7 @@ package com.example.mandirinewsapp.util
 
 class Constants {
     companion object{
-        const val API_KEY = "f0463e6c23054d68ae094a69ea9fed37"
+        const val API_KEY = "REPLACE WITH YOUR API KEY"
         const val BASE_URL ="https://newsapi.org/"
         const val SEARCH_NEWS_TIME_DELAY = "500L"
         const val QUERY_PAGE_SIZE = 20
